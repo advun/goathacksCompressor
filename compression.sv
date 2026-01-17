3df36841c489cc041280bb88e5862f79f4f2e0ed
advun <186046511+advun@users.noreply.github.com>
2026-01-16 22:06:56 -0500

    Add files via upload

--- compression.sv
+++ compression.sv
@@ -20,12 +20,32 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module compression(
-    input in
+module compression #(parameter STARTER = 0)( //starting value
+    input in, //single bit input stream
+    input clk,
+    input reset_n
     );
     import parameters::DATA_WIDTH;
     
-    reg [DATA_WIDTH-1:0] datum;
+    reg [DATA_WIDTH-1:0] storage0; //store full values for delta encoding. old value
+    reg [DATA_WIDTH-1:0] storage1; //store full values for delta encoding. new value
+    reg [DATA_WIDTH-1:0] i; //counting variable
+    
+    //accumulator (accumulates single bit input stream into storage registers)
+    always @ (posedge clk) begin
+        if (!reset_n) begin
+            storage0 <= STARTER;
+            storage1 <= 0;
+        end
+        
+        else begin
+            storage1[i] = in;
+            
+        end
+    
+    end
+    
+    
     //delta encoding
     always @ (posedge clk) begin
     
