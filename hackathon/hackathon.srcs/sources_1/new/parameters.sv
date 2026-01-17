8a3d252634e796ec3205cffcd94836618e637871
advun <cameronpwilding@gmail.com>
2026-01-17 16:41:18 -0500

    Compressor first version???

--- hackathon/hackathon.srcs/sources_1/new/parameters.sv
+++ hackathon/hackathon.srcs/sources_1/new/parameters.sv
@@ -1,5 +1,5 @@
 package parameters;
     parameter DATA_WIDTH = 16; //includes a sign bit
-    parameter SIGNAL_NUMBER = 4;
+    parameter SIGNAL_NUMBER = 4; // this can't change (with current setup of data packing)
     
 endpackage 
