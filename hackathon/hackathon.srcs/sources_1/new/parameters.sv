baf70157076e90374d43fa953c3b65cd2af307bb
advun <186046511+advun@users.noreply.github.com>
2026-01-17 20:35:29 -0500

    Add files via upload

--- hackathon/hackathon.srcs/sources_1/new/parameters.sv
+++ hackathon/hackathon.srcs/sources_1/new/parameters.sv
@@ -1,5 +1,5 @@
 package parameters;
-    parameter DATA_WIDTH = 16; //includes a sign bit
+    parameter DATA_WIDTH = 16; //includes a sign bit.  has to be a multiple of 8!
     parameter SIGNAL_NUMBER = 4; // this can't change (with current setup of data packing)
     
 endpackage 
